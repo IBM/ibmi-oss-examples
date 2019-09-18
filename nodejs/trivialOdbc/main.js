0cd57b8096982578c7965cc2e672cc09085742c4
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2019-09-18 13:13:55 -0500

    fix ODBC driver name

--- nodejs/trivialOdbc/main.js
+++ nodejs/trivialOdbc/main.js
@@ -2,7 +2,7 @@ const odbc = require('odbc');
 
 async function test() {
   try {
-    const connection = await odbc.connect('DSN=LUGDEMO;UID=LUGDEMO;PWD=RdiNumber1');
+    const connection = await odbc.connect('DSN=LUGDEMO;DRIVER=IBM i Access ODBC Driver');
     const result = await connection.query('SELECT * FROM QIWS.QCUSTCDT');
     console.log(result);
     await connection.close();
