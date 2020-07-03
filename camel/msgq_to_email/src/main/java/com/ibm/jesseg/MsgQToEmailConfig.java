9b45f96e67043d39f9cc3b1576b9caabe847c7b1
Jesse Gorzinski <jgorzins@us.ibm.com>
2020-07-03 10:38:22 -0500

    cleanup unused imports

--- camel/msgq_to_email/src/main/java/com/ibm/jesseg/MsgQToEmailConfig.java
+++ camel/msgq_to_email/src/main/java/com/ibm/jesseg/MsgQToEmailConfig.java
@@ -1,13 +1,5 @@
 package com.ibm.jesseg;
 
-import java.util.Properties;
-import java.lang.System;
-import java.net.URLEncoder;
-import java.io.BufferedReader;
-import java.io.Console;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
