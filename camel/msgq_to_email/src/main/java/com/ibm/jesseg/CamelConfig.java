529b11e453b79568453797a823c18dd7b8659fef
Jesse Gorzinski <jgorzins@us.ibm.com>
2020-07-06 09:50:41 -0500

    formatting and Javadoc improvements

--- camel/msgq_to_email/src/main/java/com/ibm/jesseg/CamelConfig.java
+++ camel/msgq_to_email/src/main/java/com/ibm/jesseg/CamelConfig.java
@@ -1,17 +1,16 @@
 package com.ibm.jesseg;
 
-import java.util.Properties;
-import java.lang.System;
-import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.Console;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+import java.util.Properties;
 
 /**
- * For main-line code, see the MainApp class.
+ * For main-line code, see the {@link MainApp} class.
  * <br>
  * This is a convenience class centered around reading values from a configuration
  * file (config.properties). Subclasses can provide URIs appropriate for establishing
@@ -22,10 +21,16 @@ import java.io.IOException;
  * configuration file.
  */
 class CamelConfig {
-    private Properties m_props = null;
-    private Console m_console = System.console();
-    private BufferedReader m_stdin = new BufferedReader(new InputStreamReader(System.in));
+    private Properties           m_props   = null;
+    private final Console        m_console = System.console();
+    private final BufferedReader m_stdin   = new BufferedReader(new InputStreamReader(System.in));
 
+    /**
+     * Whether the given property looks like a password property that should be obfuscated
+     *
+     * @param _prop the property
+     * @return whether or not it looks like a password
+     */
     private static boolean looksLikePassword(final String _prop) {
         if (null == _prop) {
             return false;
@@ -33,16 +38,31 @@ class CamelConfig {
         final String lowercase = _prop.toLowerCase();
         return lowercase.contains("pass") || lowercase.contains("pw");
     }
+
+    /**
+     * Obfuscate the property value if the property itself looks like a password property
+     * @param _prop the property name
+     * @param _value the value of the property
+     * @return the value of the property if obfuscation is not needed, otherwise an obfuscated form.
+     */
     private static Object obfuscatePropertyValue(final String _prop, final Object _value) {
-        if(looksLikePassword(_prop)) {
+        if (looksLikePassword(_prop)) {
             return "xxxxxxxxx";
         }
         return _value;
     }
 
+    /**
+     * Ask the user for the property's value
+     *
+     * @param _prop the property
+     * @return the user's input (may be empty string), or null if no input can be gathered
+     * @throws IOException
+     * @throws RuntimeException if we can't access the console to properly ask for the password in a masked manner
+     */
     private String askUser(final String _prop) throws IOException {
         final String promptText = "Enter value for '" + _prop + "': ";
-        if(null == m_console) {
+        if (null == m_console) {
             if (looksLikePassword(_prop)) {
                 throw new RuntimeException("Can't properly ask for password ('" + _prop + "' property).");
             } else {
@@ -61,13 +81,20 @@ class CamelConfig {
 
     /**
      * Returns the given property, or null if it doesn't exist
+     * @param _prop
+     * @return
+     * @throws IOException
      */
     public synchronized String getProperty(final String _prop) throws IOException {
         return getProperty(_prop, null, false);
     }
 
     /**
-     * Returns the given property. If the property is not set, the given default is returned.
+     * Returns the value for the given property. If the property is not set, the given default is returned.
+     * @param _prop the property
+     * @param _default the default value
+     * @return the value for the given property. If the property is not set, the given default is returned.
+     * @throws IOException
      */
     public synchronized String getProperty(final String _prop, final String _default) throws IOException {
         return getProperty(_prop, _default, false);
@@ -77,9 +104,14 @@ class CamelConfig {
      * Returns the given property. If the property is not set, the given default is returned.
      * If the _abortIfNoValueOrDefault parameter is true, then a RuntimeException will be thrown
      * if the property is not set and the default value is null.
+     * @param _prop the property's value
+     * @param _default the default value
+     * @param _abortIfNoValueOrDefault whether to abort (via a {@link RuntimeException}) if the value is not set and there is no default.
+     * @return
+     * @throws IOException
      */
     public synchronized String getProperty(final String _prop, final String _default, final boolean _abortIfNoValueOrDefault) throws IOException {
-        if(null == m_props) {
+        if (null == m_props) {
             m_props = new Properties();
             String propertyFile = System.getProperty("camelconfig");
             final InputStream inputStream;
@@ -95,24 +127,24 @@ class CamelConfig {
             }
         }
         Object ret = System.getProperty(_prop);
-        if(null == ret) {
+        if (null == ret) {
             ret = m_props.getProperty(_prop);
         }
-        if(null == ret && null == _default) {
+        if (null == ret && null == _default) {
             String userInput = askUser(_prop);
-            if(null != userInput && !userInput.trim().isEmpty()) {
+            if (null != userInput && !userInput.trim().isEmpty()) {
                 ret = userInput.trim();
             }
         }
-        if(null == ret) {
-            if(_abortIfNoValueOrDefault) {
+        if (null == ret) {
+            if (_abortIfNoValueOrDefault) {
                 System.err.println("ERROR: Required configuration property not set: " + _prop);
                 throw new RuntimeException("ERROR: Required configuration property not set: " + _prop);
             }
-            System.out.println(""+_prop+"="+obfuscatePropertyValue(_prop, _default) + " (default)");
+            System.out.println("" + _prop + "=" + obfuscatePropertyValue(_prop, _default) + " (default)");
             return URLEncoder.encode(_default, "UTF-8");
         }
-        System.out.println(""+_prop+"="+obfuscatePropertyValue(_prop,ret));
+        System.out.println("" + _prop + "=" + obfuscatePropertyValue(_prop, ret));
         return URLEncoder.encode(ret.toString(), "UTF-8");
     }
 }
--- camel/msgq_to_email/src/main/java/com/ibm/jesseg/MainApp.java
+++ camel/msgq_to_email/src/main/java/com/ibm/jesseg/MainApp.java
@@ -1,7 +1,5 @@
 package com.ibm.jesseg;
 
-import java.lang.System;
-
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -10,7 +8,7 @@ import org.apache.camel.impl.DefaultCamelContext;
  * A Camel Application that routes messages from an IBM i message queue to email.
  */
 public class MainApp { 
-    public static void main(String... args) throws Exception {
+    public static void main(final String... args) throws Exception {
 
         // This class simply reads values and creates URIs from the config.properties file.
         // This is not a standard Camel class, but is part of this example. You can feel free
@@ -25,6 +23,7 @@ public class MainApp {
         final String msgqUri = conf.getMsgQUri(); //something like -> jt400://username:password@localhost/qsys.lib/mylib.lib/myq.DTAQ?keyed=false&format=binary&guiAvailable=false
         final String smtpUri = conf.getSmtpUri(); //something like -> smtp://my.smtp.server.com?from=jgorzins@us.ibm.com&to=jgorzins@us.ibm.com&subject=Camel is Really Amazing!
         context.addRoutes(new RouteBuilder() {
+            @Override
             public void configure() {
                 from(msgqUri)
                 .wireTap("log:msgq_to_email?showAll=true&level=INFO") // This is just for debug data flowing through the route
