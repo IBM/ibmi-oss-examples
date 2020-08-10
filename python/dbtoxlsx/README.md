68fe1f3d633b8ed6bbf649b846e1bd4dfb8dece1
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2020-08-10 14:28:11 -0500

    refactor(dbtoxlsx): Example to use pyodbc instead of ibmdb2

--- python/dbtoxlsx/README.md
+++ python/dbtoxlsx/README.md
@@ -1,9 +1,38 @@
 
-# "DBTOXLSX" using Python 3 and ibm_db_dbi
+# DBTOXLSX
 
-# Installing requisites
-  - **ibm_db:** `yum install python3-ibm_db`
-  - **xlsxwriter** `pip3 install xlsxwriter`
+Python command line tool to create Excel workbook with data from Db2 for i table.
+
+## Prerequisites
+
+- Install Python
+
+    On IBM i this can be installed via yum from an SSH terminal with:
+
+    ```bash
+    $ yum install python3
+    ```
+
+- Install ODBC driver and required dependencies.
+    Refer to the [odbc setup guide](https://github.com/IBM/ibmi-oss-examples/blob/master/odbc/odbc.md#odbc) for platform specific instrctions.
+
+## Getting Started
+
+1) Clone this project and change directory into `ibmi-oss-example/nodejs/exceljs`
+
+    ```bash
+    git clone https://github.com/IBM/ibmi-oss-examples.git
+
+    cd ibmi-oss-examples/nodejs/exceljs
+    ```
+
+2) Install dependencies
+
+   ```bash
+   $ pip install -r requiremnets.txt
+   ```
+
+3) Configure `DB_HOST`, `DB_USER`, and `DB_PASS` enviorment variables within the [.env](.env) file. Or configure `DSN` environment variable to use a [DSN](https://github.com/IBM/ibmi-oss-examples/blob/master/odbc/odbc.md#dsns) to connect.
 
 # Example
 - `python3 dbtoxlsx.py -h`
@@ -11,21 +40,33 @@
 
 # Usage
 ```
-dbtoxlsx.py [-h] [-c C] [-l L] [-f [FNAMES [FNAMES ...]]] [-o O] [-b B] [-i I]
+usage: dbtoxlsx.py [-h] [-d DSN] [-u USER] [-p PASSWORD] [-H HOST]
+                   [-c COMMAND] [-l LIBRARY] [-f [FNAMES [FNAMES ...]]]
+                   [-o OUTPUT] [-b] [-i] [-v]
 
-Implement SQL from IBM i command line and direct output to an Excel
-spreadsheet.
+Command line tool to create an Excel workbook with data from Db2 for i table.
 
 optional arguments:
   -h, --help            show this help message and exit
-  -c C, --c C           SQL command to execute. If left empty you must specify
+  -d DSN, --dsn DSN     The DSN
+  -u USER, --user USER  The database user
+  -p PASSWORD, --password PASSWORD
+                        The database user"s password
+  -H HOST, --host HOST  The database host server. NOTE: defaults to localhost
+  -c COMMAND, --command COMMAND
+                        SQL command to execute. If left empty you must specify
                         a library and source file to execute the default
                         command: Select * from <library>.<file>
-  -l L, --l L           Name of the library that contains the database source
+  -l LIBRARY, --library LIBRARY
+                        Name of the library that contains the database source
                         file(s) that you wish to query
-  -f [FNAMES [FNAMES ...]], --f [FNAMES [FNAMES ...]]
+  -f [FNAMES [FNAMES ...]], --files [FNAMES [FNAMES ...]]
                         One or more database source files
-  -o O, --o O           Name of the excel file to contain the output
-  -b B, --b B           Turn on bold for column headings
-  -i I, --i I           Turn on italic for column headings
+  -o OUTPUT, --output OUTPUT
+                        Name of the excel file to contain the output. Defaults
+                        to IBMiSQL
+  -b, --bold            Turn on bold for column headings
+  -i, --italic          Turn on italic for column headings
+  -v, --verbose         Logs verbose output for debug purposes
+
 ```
--- python/dbtoxlsx/dbtoxlsx.py
+++ python/dbtoxlsx/dbtoxlsx.py
@@ -1,66 +1,99 @@
 #!/usr/bin/env python3
 
-#
-# You will need to implement the following requisites for this application to
-# function:
-# - Install xlsxwriter via command: pip3 install xlsxwriter
-# - Install ibm_db 2.0.5.5 or higher via PTF(s) SI61963 and SI61964
-#
-from xlsxwriter import Workbook
-import ibm_db_dbi as dbi
+import pyodbc as dbi
 import argparse
+from os import getenv
+from xlsxwriter import Workbook
+from dotenv import load_dotenv
 
-parser = argparse.ArgumentParser(description="Implement SQL from IBM i \n\
-                                 command line and direct output to an \n\
-                                 Excel spreadsheet.")
-parser.add_argument('-c', '--c',
-                    help='SQL command to execute. If left empty you must \n\
+load_dotenv()
+
+parser = argparse.ArgumentParser(description="Command line tool to create an \
+                                Excel workbook with data from Db2 for i table.")
+parser.add_argument("-d", "--dsn", default=getenv("DSN"),
+                    help="The DSN")
+parser.add_argument("-u", "--user", default=getenv("DB_USER"),
+                    help="The database user")
+parser.add_argument("-p", "--password", default=getenv("DB_PASS"),
+                    help="The database user\"s password")
+parser.add_argument("-H", "--host", default=getenv("DB_HOST") or "localhost",
+                    help="The database host server. NOTE: defaults to localhost")
+parser.add_argument("-c", "--command",
+                    help="SQL command to execute. If left empty you must \n\
                     specify a library and source file to execute the \n\
-                    default command: Select * from <library>.<file>')
-parser.add_argument('-l', '--l',
-                    help='Name of the library that contains the database \n\
-                    source file(s) that you wish to query')
-parser.add_argument('-f', '--f', action='store', dest='fNames', type=str,
-                    nargs='*', help='One or more database source files')
-parser.add_argument('-o', '--o', default="IBMiSQL",
-                    help='Name of the excel file to contain the output')
-parser.add_argument('-b', '--b', type=bool, default=False,
-                    help='Turn on bold for column headings')
-parser.add_argument('-i', '--i', type=bool, default=False,
-                    help='Turn on italic for column headings')
+                    default command: Select * from <library>.<file>")
+parser.add_argument("-l", "--library",
+                    help="Name of the library that contains the database \n\
+                    source file(s) that you wish to query")
+parser.add_argument("-f", "--files", action="store", dest="fNames", type=str,
+                    nargs="*", help="One or more database source files")
+parser.add_argument("-o", "--output", default="IBMiSQL",
+                    help="Name of the excel file to contain the output. \
+                    Defaults to IBMiSQL")
+parser.add_argument("-b", "--bold", action="store_true", default=False,
+                    help="Turn on bold for column headings")
+parser.add_argument("-i", "--italic", action="store_true", default=False,
+                    help="Turn on italic for column headings")
+
+parser.add_argument("-v", "--verbose",  action="store_true", default=False,
+                    help="Logs verbose output for debug purposes")
 
 args = parser.parse_args()
 
 
+def getConnectionString():
+    connectionString = "DSN=*LOCAL"
+
+    if args.user:
+        connectionString = f"DRIVER=IBM i Access ODBC Driver;UID={args.user};"
+
+        if not args.password:
+            raise Exception("Database user was provided without a password")
+
+        connectionString += f"PWD={args.password};SYSTEM={args.host};"
+
+    elif args.dsn:
+        connectionString = f"DSN={args.dsn}"
+    
+    if args.verbose:
+        print(f"Connection String: {connectionString}")
+
+    return connectionString
+
+
 def writeDataToExcel(args, workbook, sheetName):
     """ Write query output to Excel worksheet """
-    try:
-        conn = dbi.connect()
-        cur = conn.cursor()
-        cur.execute(args.c)
-        headers = [descr[0] for descr in cur.description]
-        format = workbook.add_format({'bold': args.b, 'italic': args.i})
-        worksheet = workbook.add_worksheet(sheetName)
-        worksheet.write_row('A1', headers, format)
-        for i, row in enumerate(cur, start=1):
-            worksheet.write_row(i, 0, row)
-    except Exception as err:
-        print('ERROR:  ' + str(err))
-
+    print(f"Connecting to {args.host}")
+    conn = dbi.connect(getConnectionString())
+    cur = conn.cursor()
+    cur.execute(args.command)
+    headers = [descr[0] for descr in cur.description]
+    format = workbook.add_format({"bold": args.bold, "italic": args.italic})
+    worksheet = workbook.add_worksheet(sheetName)
+    worksheet.write_row("A1", headers, format)
+    for i, row in enumerate(cur, start=1):
+        worksheet.write_row(i, 0, row)
 
 def main(args):
     """ Validate command line arguments and build appropriate SQL query """
-    if ".xlsx" not in args.o:
-        args.o = args.o + ".xlsx"
-    if ((args.l is None or args.fNames is None) and args.c is None):
-        print('ERROR: You must specify one library and one or more database tables when using the default SQL command')
+    print(f"{args}")
+
+    if ".xlsx" not in args.output:
+        args.output = f"{args.output}.xlsx"
+    if ((args.library is None or args.fNames is None) and args.command is None):
+        raise Exception("ERROR: You must specify one library and one or more database tables when using the default SQL command")
     else:
-        with Workbook(args.o) as workbook:
-            if args.c is not None:
+        with Workbook(args.output) as workbook:
+            if args.command is not None:
                 writeDataToExcel(args, workbook, "ibmidb")
-            if args.l is not None and args.fNames is not None:
+            if args.library is not None and args.fNames is not None:
                 for x in range(len(args.fNames)):
-                    args.c = "Select * From " + args.l + "." + args.fNames[x]
+                    args.command = f"SELECT * FROM {args.library}.{args.fNames[x]}"
                     writeDataToExcel(args, workbook, args.fNames[x])
 
-main(args)
+try:
+    main(args)
+    print(f"Succssfully wrote {args.output}")
+
+except Exception as err:
+    print(f"ERROR: {str(err)}")
