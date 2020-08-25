5a499e516df67579ac57f1c266490d5dab6f2fad
Abdirahim Musse <33973272+abmusse@users.noreply.github.com>
2020-08-25 11:15:25 -0500

    fixup! fixup! fixup! fixup! fixup! feat: Add Java POI example

--- java/poi/src/main/java/com/example/App.java
+++ java/poi/src/main/java/com/example/App.java
@@ -1,7 +1,9 @@
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.DriverManager;
+import java.sql.Types;
 
 import com.ibm.as400.access.AS400JDBCDriver;
 
@@ -29,46 +31,47 @@ public class App
       String USER = props.getProperty("DB_USER");
       String PASS = props.getProperty("DB_PASS");
       String query = "SELECT CUSNUM, LSTNAM, BALDUE, CDTLMT FROM QIWS.QCUSTCDT";
-      int numColms = 4;
 
       Class.forName("com.ibm.as400.access.AS400JDBCDriver");
 
-      System.out.println("Trying to connect...");
       Connection connection = DriverManager.getConnection(URL, USER, PASS);
       Statement statement = connection.createStatement();
       ResultSet results = statement.executeQuery(query);
+      ResultSetMetaData metadata = results.getMetaData();
+
       // create excel sheets
       Workbook workbook = WorkbookFactory.create(true);
       Sheet sheet = workbook.createSheet("Customers");
 
       // Add the header
       Row header = sheet.createRow(0);
-      header.createCell(0).setCellValue("CUSNUM");
-      header.createCell(1).setCellValue("LSTNAM");
-      header.createCell(2).setCellValue("BALDUE");
-      header.createCell(3).setCellValue("CDTLMT");
 
-      // fill in the cells
+      // fill in the headers
+      int columnCount = metadata.getColumnCount();
+      for (int i = 0; i < columnCount; i++) {
+        header.createCell(i).setCellValue(metadata.getColumnName(i + 1));
+      }
+
+      // fill in the worksheet
       for (int r = 1; results.next(); r++) {
         Row row = sheet.createRow(r);
-        for (int c = 0; c < numColms; c++) {
+        for (int c = 0; c < columnCount; c++) {
           Cell cell = row.createCell(c);
-          switch(c) {
-              case 0:
-                cell.setCellValue(results.getDouble("CUSNUM"));
-              break;
-              case 1:
-                cell.setCellValue(results.getString("LSTNAM"));
-              break;
-              case 2:
-                cell.setCellValue(results.getDouble("BALDUE"));
-              break;
-              case 3:
-                cell.setCellValue(results.getDouble("CDTLMT"));
-              break;
+          // column name and type start with index 1
+          String columnName = metadata.getColumnName(c + 1);
+          int columnType = metadata.getColumnType(c + 1);
+
+          switch (columnType) {
+            case Types.NUMERIC:
+              row.createCell(c).setCellValue(results.getDouble(columnName));
+            break;
+            case Types.CHAR:
+              row.createCell(c).setCellValue(results.getString(columnName));
+            break;
           }
         }
       }
+
       // write the xlsx file
       workbook.write(new FileOutputStream("Customers.xlsx"));
       System.out.println("Succesfully created Customers.xlsx!");
