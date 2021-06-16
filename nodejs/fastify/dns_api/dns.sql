db7c18698a4e1eea1c6b19e7b8f02fd18ab0ad0f
Jesse Gorzinski <17914061+ThePrez@users.noreply.github.com>
2021-06-16 13:44:52 -0500

    Add SQL example

---
+++ nodejs/fastify/dns_api/dns.sql
@@ -0,0 +1,17 @@
+﻿--desc: basic
+SELECT
+    SYSTOOLS.HTTPGETCLOB(
+        URL => CAST('http://localhost:8088/dns/' CONCAT 'yahoo.com' AS VARCHAR(255)),
+        HTTPHEADER => CAST(NULL AS CLOB(1K))) AS ADDR
+    FROM SYSIBM.SYSDUMMY1;    
+--desc: advanced (insert JSON_TABLE here)
+select * from JSON_TABLE(SYSTOOLS.HTTPGETCLOB(                            
+                            URL => CAST('http://localhost:8088/dns_adv/' CONCAT 
+                              'yahoo.com' CONCAT 
+                              '?dns_server=8.8.8.8' AS VARCHAR(255)),
+                            HTTPHEADER => CAST(NULL AS CLOB(1K))),
+  '$[*]' 
+  COLUMNS ( ADDR VARCHAR(100) PATH 'lax $.address',
+            TTL VARCHAR(100) PATH 'lax $.ttl',
+            TYPE VARCHAR(100) PATH 'lax $.type') ERROR ON ERROR
+  ) X
