140ebdda80b9ae7662a6330f3214b5e5e0a91cdc
Yeshua Hall <yeshua@sobo.red>
2022-06-27 11:36:45 -0700

    Remove error display overrides
    Signed-off-by: Yeshua Hall <yeshua@sobo.red>

--- php/active-jobs-db2/public/index.php
+++ php/active-jobs-db2/public/index.php
@@ -2,10 +2,6 @@
 
 declare(strict_types=1);
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
